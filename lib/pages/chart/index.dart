dbc6fb308c14d958c563cff58bb41e1e10d03c6b
王俊 <1871015627@qq.com>
2019-01-16 00:05:27 +0800

    stash

--- lib/pages/chart/index.dart
+++ lib/pages/chart/index.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
 class Chart extends StatefulWidget {
   _ChartState createState() => _ChartState();
 }
 
-class _ChartState extends State<Chart> {
+class _ChartState extends State<Chart> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  var page_index = 0;
+  void initState() { 
+    super.initState();
+  // tab控制
+   _tabController = TabController(
+    length: 3,
+    vsync: this,
+  );
+   _tabController.addListener((){
+     setState(() {
+     page_index = _tabController.index;
+   });
+  });
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,10 +30,20 @@ class _ChartState extends State<Chart> {
             Text('支出'),
             Icon(Icons.arrow_drop_down,size: 38.0,)
           ],
-        )
+        ),
+        bottom: TabBar(
+          tabs: <Widget>[
+            Text('周'),
+            Text('月'),
+            Text('年'),
+          ],
+          controller: _tabController,
+        ),
       ),
-      body:Text('图表')
+      body:Text('${page_index}')
     );
     
   }
 }
+
+
